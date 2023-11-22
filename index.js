4819c10ed90ca32f7a1dcbdea621dcd7f422634a
Martin <106612709+menkodot@users.noreply.github.com>
2023-11-22 16:54:39 +0100

    added elisteners to boxes and setup states

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const container = document.getElementById('container');
 const box = document.querySelector('.box');
-
+const boxes = document.querySelectorAll('.box');
 //Collect all boxes
-const divElements = container.querySelectorAll('div');
-console.log(divElements.length);
-
-//Added eventListener by using the event target data and checking if it contains .box class
-container.addEventListener('click', (e) => {
-  if (e.target.classList.contains('box')) {
-    e.target.style.backgroundColor = 'black';
-  };
-
-});
+const computerSelector = container.querySelectorAll('.box');
 
 //Computer-selection function
 const getComputerMark = () => {
   const n = Math.floor(Math.random() * 9);
-  return divElements[n].style.backgroundColor = 'red';
-
+  return computerSelector[n].style.backgroundColor = 'red';
 };
 
-getComputerMark();
+//Add event listeners to every box
+for (let i = 0; i < boxes.length; i++) {
+  //Set att state to false
+  boxes[i].setAttribute("tag", "false");
+
+  boxes[i].addEventListener('click', (e) => {
+    e.target.setAttribute("tag", "true");
+    console.log(e.target);
+
+  })
+}
